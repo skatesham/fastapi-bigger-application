68e172b7228b4b17001c817699e0e64d728a17ca
sham <sham.vinicius@gmail.om>
2021-07-26 19:23:31 -0300

    OS on URI database

--- app/database.py
+++ app/database.py
@@ -2,10 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
+
 SQLALCHEMY_DATABASE_URL = "postgresql://tinnova:tinnova123@localhost/tinnova"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    os.getenv("URI_DB", SQLALCHEMY_DATABASE_URL)
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
