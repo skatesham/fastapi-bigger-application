69d61e09c2b783b337181217fef8d148e94e11b2
sham <sham.vinicius@gmail.om>
2021-07-26 17:38:24 -0300

    Test with embeded database H2 and test all cases

--- app/tests/test_api/test_cars.py
+++ app/tests/test_api/test_cars.py
@@ -1,13 +1,17 @@
 from fastapi.testclient import TestClient
 
+from ..database_test import configure_test_database
+
 from ...main import app
 
+configure_test_database(app)
 
 client = TestClient(app)
 
 cars_route = "/api/v1/cars"
 
 def test_create_car():
+    ''' Create a car with success '''
     request_json = {
         "name": "Ram 3",
         "year": 2020,
@@ -16,7 +20,7 @@ def test_create_car():
     response = client.post(cars_route + "/", json=request_json)
     assert response.status_code == 201
     assert response.json() == {
-        "id": 9,
+        "id": 1,
         "name": "Ram 3",
         "year": 2020,
         "brand": "Dodge"
@@ -24,27 +28,65 @@ def test_create_car():
 
 
 def test_read_car():
+    ''' Read a created car with success '''
     request_url = cars_route + "/1"
     response = client.get(request_url)
     assert response.status_code == 200
     assert response.json() == {
         "id": 1,
-        "name": "KA",
+        "name": "Ram 3",
         "year": 2020,
-        "brand": "FORD"
+        "brand": "Dodge"
     }
 
 
 def test_read_cars():
-    request_url = cars_route + "?skip=0&limit=1"
+    ''' Read all cars paginated with success '''
+    request_url = cars_route + "?skip=0&limit=100"
     response = client.get(request_url)
     assert response.status_code == 200
     assert response.json() == [
         {
         "id": 1,
-        "name": "KA",
+        "name": "Ram 3",
         "year": 2020,
-        "brand": "FORD"
+        "brand": "Dodge"
         }
     ]
 
+
+def test_delete_car():
+    ''' Delete a car with success '''
+    request_url = cars_route + "/1"
+    response = client.delete(request_url)
+    assert response.status_code == 200
+    assert response.json() == True
+
+
+def test_read_car_not_found():
+    ''' Read a car when not found '''
+    request_url = cars_route + "/1"
+    response = client.get(request_url)
+    assert response.status_code == 404
+    assert response.json() == {
+        'detail': 'Car Model not found'
+        }
+    
+    
+def test_read_cars_not_found():
+    ''' Read all cars paginated when not found '''
+    request_url = cars_route + "?skip=0&limit=100"
+    response = client.get(request_url)
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_delete_car_not_found():
+    ''' Delete a car when not exists '''
+    request_url = cars_route + "/1"
+    response = client.delete(request_url)
+    assert response.status_code == 404
+    assert response.json() == {
+        'detail': 'Car Model not found'
+        }
+    
