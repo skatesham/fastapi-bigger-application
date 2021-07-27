43fa2ae19eab760a0a97f33ef0bf368ede48813a
sham <sham.vinicius@gmail.om>
2021-07-26 23:38:39 -0300

    insert into buyers script

--- app/test/database_test.py
+++ app/test/database_test.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.sql import text
 from ..src.database import Base
 
 from ..src.dependencies import get_db
@@ -28,7 +28,6 @@ def configure_test_database(app):
 
 
 def insert_into_car():
-    from sqlalchemy.sql import text
     with engine.connect() as con:
 
         data = ( { "id": 1, "name": "Galardo", "year": "1999", "brand": "lamborghini" },
@@ -42,7 +41,6 @@ def insert_into_car():
 
 
 def insert_into_seller():
-    from sqlalchemy.sql import text
     with engine.connect() as con:
 
         data = ( { "id": 1, "name": "João da Silva", "cpf": "69285717640", "phone": "1299871234" },
@@ -53,3 +51,39 @@ def insert_into_seller():
 
         for line in data:
             con.execute(statement, **line)
+            
+def insert_into_buyers():
+    with engine.connect() as con:
+
+        data = ( 
+                { 
+                    "id": 1,
+                    "name": "Bruce Lee",
+                    "phone": "1299871234",
+                    "address_cep": "73770-000",
+                    "address_public_place": "Muro Preto",
+                    "address_district": "Cidade Baixa",
+                    "address_city": "Alto Paraíso de Goias",
+                    "address_state": "Goias"
+                },
+                { 
+                    "id": 2,
+                    "name": "Bruce Willis",
+                    "phone": "1299874321",
+                    "address_cep": "12209-000",
+                    "address_public_place": "Prédio Vermelho",
+                    "address_district": "Centro",
+                    "address_city": "São José dos Campos",
+                    "address_state": "São Paulo"
+                  }
+        )
+
+        statement = text("""INSERT INTO buyers( 
+            id, name, phone, address_cep, address_public_place, 
+            address_district, address_city, address_state) 
+            VALUES(:id, :name, :phone, :address_cep, :address_public_place,
+            :address_district, :address_city, :address_state)""")
+
+        for line in data:
+            con.execute(statement, **line)     
+
