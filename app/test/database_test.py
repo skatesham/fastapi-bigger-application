e0e660581bd0e12b1023c128fdf1155f5fc4afea
sham <sham.vinicius@gmail.om>
2021-08-03 02:42:58 -0300

    Cars test run independently

--- app/test/database_test.py
+++ app/test/database_test.py
@@ -13,32 +13,53 @@ engine = create_engine(
 
 
 def configure_test_database(app):
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    
+    TestingSessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=engine)
+
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-    
+
     def override_get_db():
         try:
             db = TestingSessionLocal()
             yield db
         finally:
             db.close()
-    
-    app.dependency_overrides[get_db] = override_get_db    
-    
 
-def insert_into_cars():
+    app.dependency_overrides[get_db] = override_get_db
+
+
+def clear_database():
     with engine.connect() as con:
-
-        data = ( { "id": 1, "name": "Galardo", "year": "1999", "brand": "lamborghini" },
-                { "id": 2, "name": "CX40", "year": "2021", "brand": "Volvo" },
+        
+        IGNORE_CONSTRAINTS = """PRAGMA ignore_check_constraints = 0"""
+        DISABLE_IGNORE_CONSTRAINTS = """PRAGMA ignore_check_constraints = 1"""
+        
+        data = (
+            "sales",
+            "buyers",
+            "sellers",
+            "stocks",
+            "cars",
+            "users",
+            "items",
         )
 
-        statement = text("""INSERT INTO cars(id, name, year, brand) VALUES(:id, :name, :year, :brand)""")
+        statement = """DELETE FROM {table:s}"""
+
+        con.execute(IGNORE_CONSTRAINTS)
+        for line in data:
+            con.execute(statement.format(table = line))
+        con.execute(DISABLE_IGNORE_CONSTRAINTS)
+
+
+def insert_into_cars(input):
+    with engine.connect() as con:
+
+        data = (input, )
+
+        statement = text(
+            """INSERT INTO cars(id, name, year, brand) VALUES(:id, :name, :year, :brand)""")
 
         for line in data:
             con.execute(statement, **line)
@@ -47,39 +68,42 @@ def insert_into_cars():
 def insert_into_sellers():
     with engine.connect() as con:
 
-        data = ( { "id": 1, "name": "João da Silva", "cpf": "69285717640", "phone": "1299871234" },
-                { "id": 2, "name": "Maria de Souza", "cpf": "42843444128", "phone": "1299874321" },
-        )
+        data = ({"id": 1, "name": "João da Silva", "cpf": "69285717640", "phone": "1299871234"},
+                {"id": 2, "name": "Maria de Souza",
+                    "cpf": "42843444128", "phone": "1299874321"},
+                )
 
-        statement = text("""INSERT INTO sellers(id, name, cpf, phone) VALUES(:id, :name, :cpf, :phone)""")
+        statement = text(
+            """INSERT INTO sellers(id, name, cpf, phone) VALUES(:id, :name, :cpf, :phone)""")
 
         for line in data:
             con.execute(statement, **line)
-            
+
+
 def insert_into_buyers():
     with engine.connect() as con:
 
-        data = ( 
-                { 
-                    "id": 1,
-                    "name": "Bruce Lee",
-                    "phone": "1299871234",
-                    "address_cep": "73770-000",
-                    "address_public_place": "Muro Preto",
-                    "address_district": "Cidade Baixa",
-                    "address_city": "Alto Paraíso de Goias",
-                    "address_state": "Goias"
-                },
-                { 
-                    "id": 2,
-                    "name": "Bruce Willis",
-                    "phone": "1299874321",
-                    "address_cep": "12209-000",
-                    "address_public_place": "Prédio Vermelho",
-                    "address_district": "Centro",
-                    "address_city": "São José dos Campos",
-                    "address_state": "São Paulo"
-                  }
+        data = (
+            {
+                "id": 1,
+                "name": "Bruce Lee",
+                "phone": "1299871234",
+                "address_cep": "73770-000",
+                "address_public_place": "Muro Preto",
+                "address_district": "Cidade Baixa",
+                "address_city": "Alto Paraíso de Goias",
+                "address_state": "Goias"
+            },
+            {
+                "id": 2,
+                "name": "Bruce Willis",
+                "phone": "1299874321",
+                "address_cep": "12209-000",
+                "address_public_place": "Prédio Vermelho",
+                "address_district": "Centro",
+                "address_city": "São José dos Campos",
+                "address_state": "São Paulo"
+            }
         )
 
         statement = text("""INSERT INTO buyers( 
@@ -89,5 +113,4 @@ def insert_into_buyers():
             :address_district, :address_city, :address_state)""")
 
         for line in data:
-            con.execute(statement, **line)     
-
+            con.execute(statement, **line)
--- app/test/test_api/test_cars.py
+++ app/test/test_api/test_cars.py
@@ -1,57 +1,62 @@
 from fastapi.testclient import TestClient
 
-from ..database_test import configure_test_database
+from ..database_test import configure_test_database, insert_into_cars, clear_database
 
 from ...main import app
 
-configure_test_database(app)
+
+CAR_ROUTE = "/api/v1/cars"
+
+CAR_JSON = {
+    "id": 1,
+    "name": "Galardo",
+    "year": 1999,
+    "brand": "lamborghini"
+    }
+
+CAR_ERROR = { 'errors': ['car does not exist'] }
+
 
 client = TestClient(app)
 
-cars_route = "/api/v1/cars"
 
-request_json = {
-    "name": "Ram 3",
-    "year": 2020,
-    "brand": "Dodge"
-}
-
-response_json = {
-    "id": 1,
-    "name": "Ram 3",
-    "year": 2020,
-    "brand": "Dodge"
-}
-
-response_error = { 'errors': ['car does not exist'] }
+def setup_function(module):
+    configure_test_database(app)
+    clear_database() 
 
 
 def test_create_car():
     ''' Create a car with success '''
-    response = client.post(cars_route + "/", json=request_json)
+    response = client.post(CAR_ROUTE + "/", json=CAR_JSON)
     assert response.status_code == 201
-    assert response.json() == response_json
+    assert response.json() == CAR_JSON
 
 
 def test_read_car():
     ''' Read a car with success '''
-    request_url = cars_route + "/1"
+    insert_into_cars(CAR_JSON)
+    
+    request_url = CAR_ROUTE + "/1"
     response = client.get(request_url)
     assert response.status_code == 200
-    assert response.json() == response_json
+    assert response.json() == CAR_JSON
 
 
 def test_read_cars():
     ''' Read all cars paginated with success '''
-    request_url = cars_route + "?skip=0&limit=100"
+    insert_into_cars(CAR_JSON)
+    
+    request_url = CAR_ROUTE + "?skip=0&limit=100"
     response = client.get(request_url)
     assert response.status_code == 200
-    assert response.json() == [ response_json ]
+    assert response.json() == [ CAR_JSON ]
 
 
 def test_delete_car():
     ''' Delete a car with success '''
-    request_url = cars_route + "/1"
+    insert_into_cars(CAR_JSON)
+    
+    request_url = CAR_ROUTE + "/1"
     response = client.delete(request_url)
     assert response.status_code == 200
     assert response.json() == True
@@ -59,15 +64,15 @@ def test_delete_car():
 
 def test_read_car_not_found():
     ''' Read a car when not found '''
-    request_url = cars_route + "/1"
+    request_url = CAR_ROUTE + "/1"
     response = client.get(request_url)
     assert response.status_code == 404
-    assert response.json() == response_error
+    assert response.json() == CAR_ERROR
     
     
 def test_read_cars_not_found():
     ''' Read all cars paginated when not found '''
-    request_url = cars_route + "?skip=0&limit=100"
+    request_url = CAR_ROUTE + "?skip=0&limit=100"
     response = client.get(request_url)
     assert response.status_code == 200
     assert response.json() == []
@@ -75,8 +80,8 @@ def test_read_cars_not_found():
 
 def test_delete_car_not_found():
     ''' Delete a car when not exists '''
-    request_url = cars_route + "/1"
+    request_url = CAR_ROUTE + "/1"
     response = client.delete(request_url)
     assert response.status_code == 404
-    assert response.json() == response_error
-    
+    assert response.json() == CAR_ERROR
+
