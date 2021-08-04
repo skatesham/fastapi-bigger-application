a97a541be7c7c6febaa6664648890af8a60ea17d
sham <sham.vinicius@gmail.om>
2021-08-04 19:26:24 -0300

    Create fixture for cars test

---
+++ app/test/templates/car_tempĺates.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def car_json():
+    return {"id": 1, "name": "Galardo", "year": 1999, "brand": "lamborghini"}
+
+
+@pytest.fixture
+def car_not_found_error():
+    return { 'errors': ['car does not exist'] }
--- app/test/test_api/test_cars.py
+++ app/test/test_api/test_cars.py
@@ -1,20 +1,17 @@
+import pytest
+
+import json
+
 from fastapi.testclient import TestClient
 
 from ..database_test import configure_test_database, insert_into_cars, clear_database
+
+from ..templates.car_tempĺates import car_json, car_not_found_error
 
 from ...main import app
 
 
 CAR_ROUTE = "/api/v1/cars"
-
-CAR_JSON = {
-    "id": 1,
-    "name": "Galardo",
-    "year": 1999,
-    "brand": "lamborghini"
-    }
-
-CAR_ERROR = { 'errors': ['car does not exist'] }
 
 
 client = TestClient(app)
@@ -25,36 +22,36 @@ def setup_function(module):
     clear_database() 
 
 
-def test_create_car():
+def test_create_car(car_json):
     ''' Create a car with success '''
-    response = client.post(CAR_ROUTE + "/", json=CAR_JSON)
+    response = client.post(CAR_ROUTE + "/", json=car_json)
     assert response.status_code == 201
-    assert response.json() == CAR_JSON
+    assert response.json() == car_json
 
 
-def test_read_car():
+def test_read_car(car_json):
     ''' Read a car with success '''
-    insert_into_cars(CAR_JSON)
+    insert_into_cars(car_json)
     
     request_url = CAR_ROUTE + "/1"
     response = client.get(request_url)
     assert response.status_code == 200
-    assert response.json() == CAR_JSON
+    assert response.json() == car_json
 
 
-def test_read_cars():
+def test_read_cars(car_json):
     ''' Read all cars paginated with success '''
-    insert_into_cars(CAR_JSON)
+    insert_into_cars(car_json)
     
     request_url = CAR_ROUTE + "?skip=0&limit=100"
     response = client.get(request_url)
     assert response.status_code == 200
-    assert response.json() == [ CAR_JSON ]
+    assert response.json() == [ car_json ]
 
 
-def test_delete_car():
+def test_delete_car(car_json):
     ''' Delete a car with success '''
-    insert_into_cars(CAR_JSON)
+    insert_into_cars(car_json)
     
     request_url = CAR_ROUTE + "/1"
     response = client.delete(request_url)
@@ -62,12 +59,12 @@ def test_delete_car():
     assert response.json() == True
 
 
-def test_read_car_not_found():
+def test_read_car_not_found(car_not_found_error):
     ''' Read a car when not found '''
     request_url = CAR_ROUTE + "/1"
     response = client.get(request_url)
     assert response.status_code == 404
-    assert response.json() == CAR_ERROR
+    assert response.json() == car_not_found_error
     
     
 def test_read_cars_not_found():
@@ -78,10 +75,10 @@ def test_read_cars_not_found():
     assert response.json() == []
 
 
-def test_delete_car_not_found():
+def test_delete_car_not_found(car_not_found_error):
     ''' Delete a car when not exists '''
     request_url = CAR_ROUTE + "/1"
     response = client.delete(request_url)
     assert response.status_code == 404
-    assert response.json() == CAR_ERROR
+    assert response.json() == car_not_found_error
 
